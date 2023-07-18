e263d4c86f268ea0c758b894c55488b0d6f0c86c
StrugglerXIV <arbiterdenis@gmail.com>
2023-07-18 00:13:47 -0400

    make it better

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,21 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  // For Heroku deployment with JawsDB MySQL
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  // For local development or other deployment environments
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DATABASE_URL, // Use the Heroku PostgreSQL URL
     {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+      dialect: 'postgres', // Use the PostgreSQL dialect
+      protocol: 'postgres', // Use the PostgreSQL protocol
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false // Disable SSL verification
+        }
+      }
     }
   );
 }
